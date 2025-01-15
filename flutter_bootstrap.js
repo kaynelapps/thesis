0e545b445f0298a0d39933f3be3a5c80748e3dac
kaynelapps <kaynelapps@gmail.com>
2025-01-15 15:17:52 +0100

    Updated web deployment files

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2660238556"
+    serviceWorkerVersion: "779665207"
   }
 });
